847fe527be89afd747cf45b5429891eced8bafc4
henriqueignacio <henriquebartalo04@gmail.com>
2024-10-19 20:03:25 -0300

    SlideShow criado

--- src/routes/SlideShow.jsx
+++ src/routes/SlideShow.jsx
@@ -6,3 +6,26 @@ const Slideshow = () => {
     './assets/brinquedos2.webp',
     './assets/brinquedo5.webp',
     './assets/brinquedo4.webp',
+    
+    
+  ];
+  const [index, setIndex] = useState(0);
+  const time = 3000;
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, time);
+
+    return () => clearInterval(timer);
+  }, [index]);
+
+  return (
+    <div>
+      <img src={images[index]} alt="Slideshow" id="imgbanner" />
+    </div>
+  );
+};
+
+export default Slideshow;
+
