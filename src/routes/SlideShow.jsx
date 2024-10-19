37f2877583eb45426e74caf79cafaa76859e4010
henriqueignacio <henriquebartalo04@gmail.com>
2024-10-19 20:02:41 -0300

    Criando SlideShow

---
+++ src/routes/SlideShow.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+const Slideshow = () => {
+  const images = [
+    './assets/brinquedo6.webp',
+    './assets/brinquedos2.webp',
+    './assets/brinquedo5.webp',
+    './assets/brinquedo4.webp',
